5d6a938006c87157a8559ea172a950ab52d10f82
LeoGuedex <110507618+LeoGuedex@users.noreply.github.com>
2024-11-18 20:33:59 -0300

    Update Main.java

--- src/main/java/leoguedex/com/github/Main.java
+++ src/main/java/leoguedex/com/github/Main.java
@@ -3,6 +3,8 @@ package leoguedex.com.github;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Hello world 1!");
     System.out.println("Hello world 2!");
+    System.out.println("Hello world 3!");
   }
 }
